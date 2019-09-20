f204a8f390be9a2fae8b816e90136d30eaf31d7a
Andrew Camps <andrewcamps@Andrews-MacBook-Pro.local>
2019-09-20 11:23:51 -0600

    added table class for storing commands

---
+++ src/table.py
@@ -0,0 +1,17 @@
+import sys
+import tkinter
+import tkinter.ttk
+
+# Local import
+from utils import new_widget
+
+# Constant Definitions
+FRAME = tkinter.ttk.Frame
+TREEVIEW = tkinter.ttk.Treeview
+SCROLLBAR = tkinter.ttk.Scrollbar
+
+class Table(TREEVIEW):
+
+    def __init__(self):
+
+        return None
